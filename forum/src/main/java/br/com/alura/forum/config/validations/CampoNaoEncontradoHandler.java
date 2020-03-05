80d7b565d3ebc569dbb45b30f5282c2e8653c46d
Paulo Henrique Bendazzoli <pbendazzoli@lnxcit024472.cit>
2020-03-05 16:44:03 -0300

    Tratando erro de DELETE sem um id existente

--- forum/src/main/java/br/com/alura/forum/config/validations/CampoNaoEncontradoHandler.java
+++ forum/src/main/java/br/com/alura/forum/config/validations/CampoNaoEncontradoHandler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,8 +23,14 @@ public class CampoNaoEncontradoHandler {
     private MessageSource messageSource;
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ErroFormularioDTO handle(EntityNotFoundException ex){
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ErroFormularioDTO handleEntityNotFound(EntityNotFoundException ex){
+        return new ErroFormularioDTO("id", "Id informado não encontrado.");
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    public ErroFormularioDTO handleEmptyResultDataAccess(EmptyResultDataAccessException ex){
         return new ErroFormularioDTO("id", "Id informado não encontrado.");
     }
 }
