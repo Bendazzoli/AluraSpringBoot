543ecdccbd9a2087db9dcadb0c4d7ec526904bdc
Paulo Henrique Bendazzoli <pbendazzoli@lnxcit024472.cit>
2020-03-05 16:34:01 -0300

    Implementando o PUT

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,10 +1,11 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.dto.DetalhesTopicoDTO;
 import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
@@ -41,5 +43,18 @@ public class TopicosController {
 		topicoRepository.save(topico);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+	}
+
+	@GetMapping("/{id}")
+	public DetalhesTopicoDTO detalharUmTopico(@PathVariable Long id){
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalhesTopicoDTO(topico);
+	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Topico topico = form.atualizar(id, topicoRepository);
+		return ResponseEntity.ok(new TopicoDTO(topico));
 	}
 }
