4fb19a4b43f40a3030f43bd674978714b75bdbb8
Paulo Henrique Bendazzoli <pbendazzoli@lnxcit024472.cit>
2020-03-06 15:28:02 -0300

    Criacao da AutenticacaoService e uso da BCryptPasswordEncoder para senha

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,14 +1,28 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AutenticacaoService autenticacaoService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //e-mail: aluno@gmail.com
+        //senha: 123456
+
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     //Serve para configurar as autorizações: quem pode usar a URL, por exemplo
     @Override
