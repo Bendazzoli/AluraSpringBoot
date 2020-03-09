bf62d97960e6253fbc080b0ea3a6a9c36839750d
Paulo Henrique Bendazzoli <pbendazzoli@lnxcit024472.cit>
2020-03-09 09:49:15 -0300

    Gerando token com JWT

---
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @Autowired
+    AuthenticationManager oauthManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity autenticar(@RequestBody @Valid LoginForm form){
+        UsernamePasswordAuthenticationToken login = form.converter();
+
+        try {
+            Authentication authentication = oauthManager.authenticate(login);
+            String token = tokenService.gerarToken(authentication);
+
+            System.out.println("token :" + token);
+
+            return ResponseEntity.ok().build();
+        }catch (AuthenticationException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
